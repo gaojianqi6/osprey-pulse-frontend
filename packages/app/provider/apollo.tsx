ec130b6261572dba5c1f103d185886f57b8edff2
Jianqi Gao <gaojianqi6@gmail.com>
2026-03-01 18:20:06 +1300

    Remove broken code in preparation for override

--- packages/app/provider/apollo.tsx
+++
@@ -1,7 +0,0 @@
-import { ApolloProvider as Provider } from '@apollo/client'
-import { apolloClient } from 'app/api/client'
-
-export function ApolloProvider({ children }: { children: React.ReactNode }) {
-  return <Provider client={apolloClient}>{children}</Provider>
-}
-
